fe8ac3c7032b5c4f29a9dc158c1e4f658ee75758
SDiego-PC\SDiego <sevilla.diego8@gmail.com>
2018-02-18 22:11:31 -0600

    Initial commit

---
+++ app/src/main/java/com/example/sdiego/test_android/LabelUpdater.java
@@ -0,0 +1,11 @@
+package com.example.sdiego.test_android;
+
+/**
+ * Created by SDiego on 13/02/2018.
+ */
+
+public class LabelUpdater {
+    public static int doubletheValue(int value){
+        return value;
+    }
+}
